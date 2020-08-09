0f9fb730c4a6436cfdd4ac08be970753df2aad3a
Sudarshana Patil <patilsudarshana94@gmail.com>
2020-08-09 18:41:35 +0530

    heroku deployment changes

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = require('./app')
 // import app from './app';
 const conf = require('./conf/production.json');
 const { api, mongo } = conf;
-const API_SERVER_PORT = api.port;
+const API_SERVER_PORT = process.env.PORT || Iapi.port;
 // const DB_URI = `mongodb://${mongo.host}:${mongo.port}/${mongo.db}`;
       
 const DB_URI='mongodb+srv://sudarshana:sudri@123@movies.5aua6.mongodb.net/movieDataset?retryWrites=true&w=majority'
