d7971e419af366dfd79a5a5d548022b4587df4b7
Sudarshana Patil <patilsudarshana94@gmail.com>
2020-08-09 18:36:28 +0530

    production URL added for mongo

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const json = require('body-parser');
+const bodyParser = require('body-parser');
 var cors = require('cors')
 var app = express();
 const adminRoute = require('./routes/admin');
 const userRoutes = require('./routes/user');
 
 // console.log(userRoutes,"routes")
-app.use(json());
+// app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+// app.use(json());
 app.use(cors());
 
 app.use('/admin', adminRoute);
--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var mongoose = require('mongoose');
 
 const app = require('./app')
 // import app from './app';
-const conf = require('./conf/development.json');
+const conf = require('./conf/production.json');
 const { api, mongo } = conf;
 const API_SERVER_PORT = api.port;
-
-const DB_URI = `mongodb://${mongo.host}:${mongo.port}/${mongo.db}`;
-
+// const DB_URI = `mongodb://${mongo.host}:${mongo.port}/${mongo.db}`;
+      
+const DB_URI='mongodb+srv://sudarshana:sudri@123@movies.5aua6.mongodb.net/movieDataset?retryWrites=true&w=majority'
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
   console.log('Connected to mongo');
   app.listen(API_SERVER_PORT, () => {
