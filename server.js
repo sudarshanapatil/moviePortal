5989d0f655a6d3717365a8e358fed113a44f94e9
Sudarshana Patil <patilsudarshana94@gmail.com>
2020-08-10 23:31:29 +0530

    env port

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const app = require('./app')
 const conf = require('./conf/development.json');
 const { api, mongo } = conf;
-const API_SERVER_PORT = api.port || process.env.PORT;;
+const API_SERVER_PORT = process.env.PORT || api.port;
 
-const DB_URI='mongodb+srv://sudarshana:sudri@123@movies.5aua6.mongodb.net/movieDataset?retryWrites=true&w=majority'
+const DB_URI = 'mongodb+srv://sudarshana:sudri@123@movies.5aua6.mongodb.net/movieDataset?retryWrites=true&w=majority'
 // const DB_URI = `mongodb://${mongo.host}:${mongo.port}/${mongo.db}`;
 
 mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
